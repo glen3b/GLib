0d140ba9edc8cfea29177f1c57522369e88f30b9
Glen Husman <wappokickingdeer@gmail.com>
2013-07-08 10:32:46 -0700

    Added GamePadManager content
    Button events and DPad events are still TODO

--- GlibXbox/XNA/InputLib/GamePadManager.cs
+++ GlibXbox/XNA/InputLib/GamePadManager.cs
@@ -3,9 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Glib.XNA.InputLib
 {
+    /// <summary>
+    /// A class containing DPad related Xbox 360 controller events.
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class DPadEvents
+    {
+        internal DPadEvents()
+        {
+
+        }
+
+        internal void FireEvents(GamePadDPad currentButtonState, GamePadDPad lastButtonState)
+        {
+            //TODO
+        }
+    }
+
+    /// <summary>
+    /// A class containing button related Xbox 360 controller events.
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class ControllerButtonEvents
+    {
+        internal ControllerButtonEvents()
+        {
+
+        }
+
+        internal void FireEvents(GamePadButtons currentButtonState, GamePadButtons lastButtonState)
+        {
+            //TODO
+        }
+    }
+
     /// <summary>
     /// A manager of GamePad related information. This class cannot be inherited.
     /// </summary>
@@ -66,13 +101,129 @@ namespace Glib.XNA.InputLib
             get { return _player; }
         }
 
+        private ControllerButtonEvents _buttons = new ControllerButtonEvents();
+
+        /// <summary>
+        /// Gets a class containing button-related Xbox 360 controller events.
+        /// </summary>
+        public ControllerButtonEvents Buttons
+        {
+            get { return _buttons; }
+        }
+
+
+        private DPadEvents _dpad = new DPadEvents();
+
+        /// <summary>
+        /// Gets a class containing DPad-related Xbox 360 controller events.
+        /// </summary>
+        public DPadEvents DPad
+        {
+            get { return _dpad; }
+        }
+        
+
+        /// <summary>
+        /// An event fired when the left joystick has moved.
+        /// </summary>
+        public event EventHandler LeftJoystickMoved;
+
+        /// <summary>
+        /// An event fired when the left trigger has moved.
+        /// </summary>
+        public event EventHandler LeftTriggerMoved;
+
+        /// <summary>
+        /// An event fired when the right trigger has moved.
+        /// </summary>
+        public event EventHandler RightTriggerMoved;
+
+        /// <summary>
+        /// An event fired when the right joystick has moved.
+        /// </summary>
+        public event EventHandler RightJoystickMoved;
+
+        /// <summary>
+        /// An event fired when the GamePad has been disconnected.
+        /// </summary>
+        public event EventHandler GamePadDisconnected;
+
+        /// <summary>
+        /// An event fired when the GamePad has been connected.
+        /// </summary>
+        public event EventHandler GamePadConnected;
+
+        /// <summary>
+        /// An event fired after the completion of the update of this GamePadManager, but before Last is assigned.
+        /// </summary>
+        public event EventHandler Updated;
+
         /// <summary>
         /// Update the GamePadManager.
         /// </summary>
         public void Update()
         {
-            //TODO
+            _currentGamepadState = GamePad.GetState(_player);
+            if (LeftJoystickMoved != null && _currentGamepadState.ThumbSticks.Left != _lastGamepadState.ThumbSticks.Left)
+            {
+                LeftJoystickMoved(this, EventArgs.Empty);
+            }
+
+            if (RightJoystickMoved != null && _currentGamepadState.ThumbSticks.Right != _lastGamepadState.ThumbSticks.Right)
+            {
+                RightJoystickMoved(this, EventArgs.Empty);
+            }
+
+            if (GamePadConnected != null && _currentGamepadState.IsConnected && !_lastGamepadState.IsConnected)
+            {
+                GamePadConnected(this, EventArgs.Empty);
+            }
+            else if (GamePadDisconnected != null && _lastGamepadState.IsConnected && !_currentGamepadState.IsConnected)
+            {
+                GamePadDisconnected(this, EventArgs.Empty);
+            }
+
+            if (LeftTriggerMoved != null && _currentGamepadState.Triggers.Left != _lastGamepadState.Triggers.Left)
+            {
+                LeftTriggerMoved(this, EventArgs.Empty);
+            }
+
+            if (RightTriggerMoved != null && _currentGamepadState.Triggers.Right != _lastGamepadState.Triggers.Right)
+            {
+                RightTriggerMoved(this, EventArgs.Empty);
+            }
+
+            _dpad.FireEvents(_currentGamepadState.DPad, _lastGamepadState.DPad);
+            _buttons.FireEvents(_currentGamepadState.Buttons, _lastGamepadState.Buttons);
+
+            if (Updated != null)
+            {
+                Updated(this, EventArgs.Empty);
+            }
+
+            _lastGamepadState = _currentGamepadState;
         }
+
+        private GamePadState _currentGamepadState = new GamePadState();
+
+        /// <summary>
+        /// Gets the current GamePad state for this player.
+        /// </summary>
+        public GamePadState Current
+        {
+            get { return _currentGamepadState; }
+        }
+
+        private GamePadState _lastGamepadState = new GamePadState();
+
+        /// <summary>
+        /// Gets the last GamePad state for this player.
+        /// </summary>
+        public GamePadState Last
+        {
+            get { return _lastGamepadState; }
+        }
+        
 
         private GamePadManager(PlayerIndex player)
         {
