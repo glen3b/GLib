8b3d4b4dfb131f3d73661cdbc9d023e78b106b95
Glen Husman <wappokickingdeer@gmail.com>
2013-07-25 17:59:55 -0700

    Added a missed using

--- ToInt/XNA/SpriteLib/TextBoxSprite.cs
+++ ToInt/XNA/SpriteLib/TextBoxSprite.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Glib.XNA.SpriteLib
 {
