864b2401ebab8ae1f62e6ba7e44446c8097798e8
Glen Husman <wappokickingdeer@gmail.com>
2013-07-27 16:43:22 -0700

    Added beginnings of networking test

---
+++ NetworkTest/NetworkTest/Game1.cs
@@ -0,0 +1,267 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Glib;
+using Glib.XNA;
+using Glib.XNA.InputLib;
+using Glib.XNA.SpriteLib;
+
+namespace NetworkTest
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        ScreenManager allScreens;
+
+        AvailableNetworkSessionCollection availableSessions;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            Components.Add(new GamerServicesComponent(this));
+            Components.Add(new InputManagerComponent(this));
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        SpriteFont font;
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("SpriteFont1");
+            
+            Screen title = new Screen(spriteBatch, Color.CornflowerBlue);
+            TextSprite createSession = new TextSprite(spriteBatch, font, "Host session");
+            createSession.IsHoverable = true;
+            createSession.NonHoverColor = Color.Black;
+            createSession.HoverColor = Color.White;
+            createSession.Pressed += new EventHandler(createSession_Pressed);
+            TextSprite joinSession = new TextSprite(spriteBatch, font, "Join session");
+            joinSession.Y += createSession.Font.LineSpacing;
+            joinSession.IsHoverable = true;
+            joinSession.NonHoverColor = Color.Black;
+            joinSession.HoverColor = Color.White;
+            joinSession.Pressed += new EventHandler(joinSession_Pressed);
+            title.AdditionalSprites.Add(createSession);
+            title.AdditionalSprites.Add(joinSession);
+            title.Name = "titleScreen";
+            title.Visible = true;
+
+            Screen waitForPlayers = new Screen(spriteBatch, Color.CornflowerBlue);
+            waitForPlayers.Name = "playerList";
+
+            Screen listSessions = new Screen(spriteBatch, Color.CornflowerBlue);
+            listSessions.Name = "listSessions";
+            TextSprite refreshSessions = new TextSprite(spriteBatch, font, "Refresh");
+            refreshSessions.IsHoverable = true;
+            refreshSessions.NonHoverColor = Color.Black;
+            refreshSessions.HoverColor = Color.LimeGreen;
+            refreshSessions.Pressed += new EventHandler(joinSession_Pressed);
+            listSessions.AdditionalSprites.Add(refreshSessions);
+
+            allScreens = new ScreenManager(spriteBatch, Color.Red, title, waitForPlayers, listSessions);
+            // TODO: use this.Content to load your game content here
+        }
+
+        void joinSession_Pressed(object sender, EventArgs e)
+        {
+            if (!Guide.IsVisible)
+            {
+                availableSessions = NetworkSession.Find(
+        NetworkSessionType.SystemLink, maximumLocalPlayers,
+        null);
+                allScreens["listSessions"].Visible = true;
+                allScreens["titleScreen"].Visible = false;
+                allScreens["listSessions"].AdditionalSprites.RemoveAll(ts => ts.GetType() != typeof(TextSprite) || ts.Cast<TextSprite>().HoverColor != Color.LimeGreen);
+                float y = 50;
+                float x = 100;
+                for (int sessionIndex = 0; sessionIndex < availableSessions.Count; sessionIndex++)
+                {
+                    AvailableNetworkSession availableSession =
+                        availableSessions[sessionIndex];
+
+                    string HostGamerTag = availableSession.HostGamertag;
+                    int GamersInSession = availableSession.CurrentGamerCount;
+                    SessionInfoDisplay info = new SessionInfoDisplay(spriteBatch, new Vector2(x, y), font, HostGamerTag + ": " + GamersInSession + "/2 gamers", availableSession);
+                    info.IsHoverable = true;
+                    info.NonHoverColor = Color.Black;
+                    info.HoverColor = Color.White;
+                    info.Pressed += new EventHandler(info_Pressed);
+                    allScreens["listSessions"].AdditionalSprites.Add(info);
+                }
+            }
+        }
+
+        void info_Pressed(object sender, EventArgs e)
+        {
+            if (!Guide.IsVisible)
+            {
+                AvailableNetworkSession asession = sender.Cast<SessionInfoDisplay>().Session;
+                this.session = NetworkSession.Join(asession);
+            }
+        }
+
+        void createSession_Pressed(object sender, EventArgs e)
+        {
+            if (!Guide.IsVisible)
+            {
+
+                session = NetworkSession.Create(
+        NetworkSessionType.SystemLink,
+        maximumLocalPlayers, maximumGamers, privateGamerSlots,
+        null);
+                session.AllowJoinInProgress = true;
+
+                allScreens["titleScreen"].Visible = false;
+                allScreens["playerList"].Visible = true;
+                session.GamerJoined += new EventHandler<GamerJoinedEventArgs>(session_GamerJoined);
+
+                /*
+                Texture2D newGamerImage = Texture2D.FromStream(GraphicsDevice, Gamer.SignedInGamers[0].GetProfile().GetGamerPicture());
+                Vector2 pos = new Vector2(100, 50);
+                Sprite gamerIcon = new Sprite(newGamerImage, pos, spriteBatch);
+                allScreens["playerList"].Sprites.Add(gamerIcon);
+                TextSprite gamerName = new TextSprite(spriteBatch, new Vector2(pos.X + gamerIcon.Width + 5, pos.Y), font, Gamer.SignedInGamers[0].DisplayName == null ? Gamer.SignedInGamers[0].Gamertag : Gamer.SignedInGamers[0].DisplayName);
+                allScreens["playerList"].AdditionalSprites.Add(gamerName);
+                */
+            }
+        }
+
+        void session_GamerJoined(object sender, GamerJoinedEventArgs e)
+        {
+            Screen playerList = allScreens["playerList"];
+            Texture2D newGamerImage = Texture2D.FromStream(GraphicsDevice, e.Gamer.GetProfile().GetGamerPicture());
+            Vector2 pos = new Vector2(100, 50);
+            foreach (Sprite s in playerList.Sprites)
+            {
+                pos.Y += s.Height + 5;
+            }
+            Sprite gamerIcon = new Sprite(newGamerImage, pos, spriteBatch);
+            playerList.Sprites.Add(gamerIcon);
+            TextSprite gamerName = new TextSprite(spriteBatch, new Vector2(pos.X + gamerIcon.Width + 5, pos.Y), font, e.Gamer.DisplayName == null ? e.Gamer.Gamertag : e.Gamer.DisplayName);
+            allScreens["playerList"].AdditionalSprites.Add(gamerName);
+            if (session.AllGamers.Count >= 2)
+            {
+                //Todo
+                //session.StartGame();
+            }
+        }
+
+        NetworkSession session;
+
+        int maximumGamers = 2;
+        int privateGamerSlots = 1;
+        int maximumLocalPlayers = 1;
+
+        bool isAsyncingIn = false;
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        void showSignIn(IAsyncResult res)
+        {
+            isAsyncingIn = true;
+            Guide.EndShowMessageBox(res);
+        }
+
+        bool hasSignedIn = false;
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (Gamer.SignedInGamers.Count == 0 && !Guide.IsVisible && !hasSignedIn)
+            {
+                Guide.ShowSignIn(2, true);
+                hasSignedIn = true;
+            }
+            else if (!isAsyncingIn && Gamer.SignedInGamers.Count == 0 && !Guide.IsVisible)
+            {
+                AsyncCallback showDialog = new AsyncCallback(showSignIn);
+                Guide.BeginShowMessageBox("Must be signed in", "This game requires a signed in gamer to play.", new string[]{"OK"}, 0, MessageBoxIcon.Alert, showDialog, Guid.NewGuid());
+            }
+
+            if (isAsyncingIn && Gamer.SignedInGamers.Count > 0)
+            {
+                isAsyncingIn = false;
+                
+            }
+            else if (isAsyncingIn && !Guide.IsVisible)
+            {
+                Guide.ShowSignIn(2, true);
+            }
+
+            allScreens.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override bool BeginDraw()
+        {
+            allScreens.BeginDraw();
+            return base.BeginDraw();
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            allScreens.Draw();
+
+            base.Draw(gameTime);
+        }
+
+        protected override void EndDraw()
+        {
+            allScreens.EndDraw();
+            base.EndDraw();
+        }
+    }
+}
---
+++ NetworkTest/NetworkTest/SessionInfoDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glib.XNA.SpriteLib;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Net;
+
+namespace NetworkTest
+{
+    public class SessionInfoDisplay : TextSprite
+    {
+        public AvailableNetworkSession Session;
+
+        public SessionInfoDisplay(SpriteBatch s, Vector2 v, SpriteFont f, string t, AvailableNetworkSession sess)
+            : base(s, v, f, t)
+        {
+            Session = sess;
+        }
+    }
+}
