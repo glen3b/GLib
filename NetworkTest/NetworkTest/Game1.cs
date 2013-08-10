96829063bde7c79a1cda4b017f90404a746c1c80
Glen Husman <wappokickingdeer@gmail.com>
2013-08-10 09:00:50 -0700

    Added Binaries folder

--- NetworkTest/NetworkTest/Game1.cs
+++ NetworkTest/NetworkTest/Game1.cs
@@ -1,18 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using Glib;
 using Glib.XNA;
 using Glib.XNA.InputLib;
 using Glib.XNA.SpriteLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Net;
 
 namespace NetworkTest
 {
