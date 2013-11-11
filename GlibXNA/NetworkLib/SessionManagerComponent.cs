bed2d587721f104f18ce46fc9b752b7b3e143d53
Glen Husman <wappokickingdeer@gmail.com>
2013-11-10 16:48:47 -0800

    Added SessionManagerComponent

---
+++ GlibXNA/NetworkLib/SessionManagerComponent.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Glib.XNA.NetworkLib
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class SessionManagerComponent : Microsoft.Xna.Framework.GameComponent
+    {
+        public SessionManagerComponent(Game game)
+            : base(game)
+        {
+            throw new NotImplementedException();
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+            throw new NotImplementedException();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+            throw new NotImplementedException();
+            base.Update(gameTime);
+        }
+    }
+}
