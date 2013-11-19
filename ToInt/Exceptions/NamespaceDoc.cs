7536e69f150215e9eeb1e1f9f006c3e54aafa9ea
glen3b <wappokickingdeer@gmail.com>
2013-11-19 15:45:04 -0800

    Fixe namespace doc type for Exceptions namespace

--- ToInt/Exceptions/NamespaceDoc.cs
+++ ToInt/Exceptions/NamespaceDoc.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Glib.Exceptions
 {
     /// <summary>
-    /// The <see cref="Glib.Exceptions"/> namespace provides various extensions not provided by the .NET framework.
+    /// The <see cref="Glib.Exceptions"/> namespace provides various exception classes that are not provided by the .NET framework.
     /// </summary>
     [System.Runtime.CompilerServices.CompilerGenerated]
     class NamespaceDoc
