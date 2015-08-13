d1431a246a6a43f87ad2c49fe5b189c882ef2f8d
Glen Husman <wappokickingdeer@gmail.com>
2015-08-13 12:03:38 -0700

    Added winforms extensions for ProgressBar

---
+++ ToInt/WinForms/WindowsFormsExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Glib.WinForms
+{
+    public enum ProgressBarState : int
+    {
+        Normal = 1,
+        Error = 2,
+        Paused = 3,
+    }
+
+    public static class WindowsFormsExtensions
+    {
+        const int WM_USER = 0x400;
+        const int PBM_SETSTATE = WM_USER + 16;
+        const int PBM_GETSTATE = WM_USER + 17;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        public static ProgressBarState GetState(this ProgressBar pBar)
+        {
+            return (ProgressBarState)(int)SendMessage(pBar.Handle, PBM_GETSTATE, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public static void SetState(this ProgressBar pBar, ProgressBarState state)
+        {
+            SendMessage(pBar.Handle, PBM_SETSTATE, (IntPtr)state, IntPtr.Zero);
+        } 
+    }
+}
