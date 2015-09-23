f44d6e2f50da61c13dbc781cad3f8555b5d35ea5
Glen Husman <wappokickingdeer@gmail.com>
2015-09-22 18:36:25 -0700

    Added interface docs for IObjectPool

--- GlibXNA/IObjectPool.cs
+++ GlibXNA/IObjectPool.cs
@@ -4,8 +4,21 @@ namespace Glib.XNA
     public interface IObjectPool<TPooled>
      where TPooled : IResettable
     {
-        TPooled GetObject();
-        void ReturnObject(TPooled element);
-        int Size { get; }
+        /// <summary>
+        /// Gets an object from the pool.
+        /// </summary>
+        /// <returns>A useable object from the object pool.</returns>
+        public TPooled GetObject();
+
+        /// <summary>
+        /// Returns an object that was created by this pool to the pool to be reset.
+        /// </summary>
+        /// <param name="element">The object to return to the pool.</param>
+        public void ReturnObject(TPooled element);
+
+        /// <summary>
+        /// The total number of elements tracked by the pool.
+        /// </summary>
+        public int Size { get; }
     }
 }
